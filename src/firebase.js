48536172daa81080a6da1b65815a2572d1c3709b
harman <harmandeeps008@gmail.com>
2021-08-16 10:50:02 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-rbJqjMaw6l986C4uV6X9RUDfy4-p0gs",
-  authDomain: "reels-fbfc2.firebaseapp.com",
-  projectId: "reels-fbfc2",
-  storageBucket: "reels-fbfc2.appspot.com",
-  messagingSenderId: "398940743140",
-  appId: "1:398940743140:web:78877227afdc9101c493f1",
+ 
 };
 
 firebase.initializeApp(firebaseConfig);
